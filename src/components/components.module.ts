2c5f541e231805a1abcfd53ec54083568eaf2486
gustavo2048 <gustavo_20034@hotmail.com>
2018-10-08 18:13:44 -0300

    componente listado pacientes

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,13 @@
+import { IonicModule } from 'ionic-angular';
 import { NgModule } from '@angular/core';
 import { CardBuscadorComponent } from './card-buscador/card-buscador';
+import { PacienteListaComponent } from './paciente-lista/paciente-lista';
 @NgModule({
-	declarations: [CardBuscadorComponent],
-	imports: [],
-	exports: [CardBuscadorComponent]
+	declarations: [CardBuscadorComponent,
+    PacienteListaComponent],
+	imports: [IonicModule.forRoot(CardBuscadorComponent,
+		PacienteListaComponent)],
+	exports: [CardBuscadorComponent,
+    PacienteListaComponent]
 })
 export class ComponentsModule {}
