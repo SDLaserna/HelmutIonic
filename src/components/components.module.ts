eb90a87d30a945fbfa7604cf4eb12f40775917c1
SDLaserna <xerox90913@gmail.com>
2018-10-08 18:11:06 -0300

    Vista Card-buscador

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CardBuscadorComponent } from './card-buscador/card-buscador';
+import { IonicModule } from 'ionic-angular';
 @NgModule({
 	declarations: [CardBuscadorComponent],
-	imports: [],
+	imports: [IonicModule.forRoot(CardBuscadorComponent)],
 	exports: [CardBuscadorComponent]
 })
 export class ComponentsModule {}
